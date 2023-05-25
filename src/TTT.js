ebce38fca529237f08634fdec3f988ddbdb507d9
Samuel <sixgod666@gmail.com>
2023-05-25 11:31:51 -0500

    222

--- src/TTT.js
+++ src/TTT.js
@@ -3,38 +3,56 @@ const TOTAL_NUM = 9;
 
 
 function TTT() {
-const [nextStep, setNextStep] = useState("O");
-const [pieces, SetPieces] = useState([]);
+    const [nextStep, setNextStep] = useState("O");
+    const [pieces, SetPieces] = useState([]);
+    const [isEnd, SetIsEnd] = useState(false);
 
-useEffect(()=>{
-    let initPieces = new Array(TOTAL_NUM);
+    useEffect(() => {
+        let initPieces = new Array(TOTAL_NUM);
 
-    for(let i=0; i< TOTAL_NUM; i++){
-        initPieces[i] = {id: i, content: " ", isClick: false};
+        for (let i = 0; i < TOTAL_NUM; i++) {
+            initPieces[i] = { id: i, content: " ", isClick: false };
+        }
+        SetPieces(initPieces);
+
+    }, []);
+
+    function isWin(piece) {
+        let n = Math.sqrt(TOTAL_NUM);
+        let x = Math.floor(piece.id / n);
+        let y = Math.floor(piece.id % n);
+        console.log(`x=${x} y=${y}`);
     }
-},[]);
 
-function handleClick(piece){
-    setNextStep(prevStep => prevStep==="O"?"X":"O");
-    pieces.map((curPiece)=>{
-        if(curPiece.id===piece.id){
-            curPiece.content = nextStep;
+    function handleClick(piece) {
+        if (piece.isClick === true) {
+            console.log(`id: ${piece.id} is clicked`);
+            return;
         }
-        return curPiece;
-    })
-}
+        setNextStep(prevStep => prevStep === "O" ? "X" : "O");
 
-return(
-    <div>
-        {
-            pieces.map((curPiece)=>{
-                return <button onClick={() =>handleClick(curPiece)}>{curPiece.content}</button>
-            })
-        }
-    
-    </div>
-)
+        isWin(piece);
 
+        pieces.map((curPiece) => {
+            if (!curPiece.isClick && curPiece.id === piece.id) {
+                curPiece.content = nextStep;
+                curPiece.isClick = true;
+            }
+            return curPiece;
+        })
+    }
 
+    return (
+        <>
+            <div className="board">
+                {
+                    pieces.map((curPiece) => {
+                        return <button className="piece" key={curPiece.id} onClick={() => handleClick(curPiece)}>{curPiece.content}</button>
+                    })
+                }
+            </div>
+            {!isEnd && <p>{nextStep === "O" ? "X" : "O"} <span>wins!</span></p>}
+        </>
+    )
 }
 export default TTT;
