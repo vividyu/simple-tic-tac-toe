bb970287ebc6bf5e1d29c1ca70b47d690f52b512
Samuel <sixgod666@gmail.com>
2023-05-25 12:59:48 -0500

    333

--- src/TTT.js
+++ src/TTT.js
@@ -3,7 +3,7 @@ const TOTAL_NUM = 9;
 
 
 function TTT() {
-    const [nextStep, setNextStep] = useState("O");
+    const [curStep, setCurStep] = useState("-");
     const [pieces, SetPieces] = useState([]);
     const [isEnd, SetIsEnd] = useState(false);
 
@@ -17,29 +17,61 @@ function TTT() {
 
     }, []);
 
-    function isWin(piece) {
-        let n = Math.sqrt(TOTAL_NUM);
-        let x = Math.floor(piece.id / n);
-        let y = Math.floor(piece.id % n);
-        console.log(`x=${x} y=${y}`);
+    function isWin(piece, newStep) {
+        let n = Math.floor(Math.sqrt(TOTAL_NUM));
+        let cur_x = Math.floor(piece.id / n);
+        let cur_y = Math.floor(piece.id % n);
+
+        console.log("newStep=" + newStep + " curX=" + cur_x + " curY=" + cur_y);
+
+        let xCount = 0, yCount = 0, DiagonalCount = 0;
+
+        for (let i = 0; i < TOTAL_NUM; i++) {
+            let x = Math.floor(i / n);
+            let y = Math.floor(i % n);
+
+            if (x === cur_x && pieces[i].content === newStep) {
+                xCount++;
+            }
+            if (y === cur_y && pieces[i].content === newStep) {
+                yCount++;
+            }
+
+            if ((x === y || x + y === n - 1) && pieces[i].content === newStep) {
+                DiagonalCount++;
+            }
+
+        }
+        console.log(`xCount=${xCount}; yCount=${xCount}; DiagonalCount=${DiagonalCount};`);
+
+        if (xCount === n || xCount === n || DiagonalCount === n) {
+            return true;
+        }
+
+        // no winner
+        return false;
     }
+
 
     function handleClick(piece) {
         if (piece.isClick === true) {
             console.log(`id: ${piece.id} is clicked`);
             return;
         }
-        setNextStep(prevStep => prevStep === "O" ? "X" : "O");
 
-        isWin(piece);
+        const newStep = curStep === "O" ? "X" : "O";
+        setCurStep(newStep);
 
         pieces.map((curPiece) => {
             if (!curPiece.isClick && curPiece.id === piece.id) {
-                curPiece.content = nextStep;
+                curPiece.content = newStep;
                 curPiece.isClick = true;
             }
             return curPiece;
         })
+
+        const winStat = isWin(piece, newStep);
+        if (winStat) SetIsEnd(true);
     }
 
     return (
@@ -51,7 +83,7 @@ function TTT() {
                     })
                 }
             </div>
-            {!isEnd && <p>{nextStep === "O" ? "X" : "O"} <span>wins!</span></p>}
+            {isEnd && (<p>{curStep} <span>wins!</span></p>)}
         </>
     )
 }
